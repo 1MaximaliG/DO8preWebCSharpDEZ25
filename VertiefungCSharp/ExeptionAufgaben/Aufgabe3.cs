dfb0e0818f5b5e291374102df2c50bd8cee1c267
Max Gronemann <Max.Gronemann@oc.bitlc.de>
2025-12-03 15:35:48 +0100

    Aufgabe3 Generics/Collections  + nachtrag extensionmethods aufgabe 3

---
+++ VertiefungCSharp/ExeptionAufgaben/Aufgabe3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExeptionAufgaben
+{
+    public static class CopyrightClass
+    {
+        //powerd by https://github.com/SteffenKloempges
+        public static void CreateCopyright(this string text)
+        {
+            string[] textToArr = text.Split(" ");
+            for (int i = 0; i < textToArr.Length; i++)
+            {
+                if (i == 0 || i % 10 == 0)
+                {
+                    System.Console.Write("Bitlc Dortmund ");
+                }
+                else
+                {
+                    System.Console.Write(textToArr[i] + " ");
+                }
+            }
+        }
+    }
+    public static class Aufgabe3
+    {
+        public static void TuWas()
+        {
+            var abc = Enum.GetNames<ABC>();//Test für SourceTree
+
+        }
+    }
+    public enum ABC
+    {
+        A, B, C
+    }
+}
--- VertiefungCSharp/GenericsUndCollections/Aufgabe01.cs
+++ VertiefungCSharp/GenericsUndCollections/Aufgabe01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security;
 using System.Text;
 
 namespace GenericsUndCollections
@@ -16,24 +17,53 @@ namespace GenericsUndCollections
             + "35;E;Anne Kunze;Bremen";
 
             string[] zeile = text.Split("\n");
+            //foreach (string wort in zeile)
+            //{
+            //    string[] parts = wort.Split(";");
+            //    Console.WriteLine($"Zimmer {parts[0]}:");
+            //    //umwandlung von Buchstabe in Wort
+            //    if (parts[1] == "D")
+            //        Console.WriteLine($"Doppelzimmer");
+            //    else
+            //        Console.WriteLine($"Einzelzimmer");
+                
+            //    //Aufteilen in Vor- und Nachname
+            //    string[] name = parts[2].Split(' ');
+            //    Console.WriteLine($"Vorname: {name[0]}");
+            //    Console.WriteLine($"Nachname: {name[1]}");
+
+            //    Console.WriteLine($"Wohnort: {parts[3]}");
+            //}
+            //übertragen in ein Dictionary
+            Dictionary<string, string> besucher;
+            List<Dictionary<string, string>> belegungsListe = new List<Dictionary<string, string>>();
             foreach (string wort in zeile)
             {
+                besucher = new Dictionary<string, string>();
                 string[] parts = wort.Split(";");
-                Console.WriteLine($"Zimmer {parts[0]}:");
+                besucher.Add("Zimmer: ",parts[0]);
                 //umwandlung von Buchstabe in Wort
                 if (parts[1] == "D")
-                    Console.WriteLine($"Doppelzimmer");
+                    besucher.Add("","Doppelzimmer");
                 else
-                    Console.WriteLine($"Einzelzimmer");
-
+                    besucher.Add("","Einzelzimmer");
                 //Aufteilen in Vor- und Nachname
                 string[] name = parts[2].Split(' ');
-                Console.WriteLine($"Vorname: {name[0]}");
-                Console.WriteLine($"Nachname: {name[1]}");
+                besucher.Add("Vorname: ",name[0]);
+                besucher.Add("Nachname: ",name[1]);
 
-                Console.WriteLine($"Wohnort: {parts[3]}");
+                besucher.Add("Wohnort: ",parts[3]);
+                besucher["Zimmer: "] = "lhfahhfla";
+                belegungsListe.Add(besucher);
             }
-            //
+            //Ausgabe List<Dictionary>
+            foreach(var item in belegungsListe)
+            {
+                foreach(var feld in item)
+                {
+                    Console.WriteLine(feld.Key + feld.Value);
+                }
+            }
         }
     }
 }
