75ee19a5c5f0015eaf0c8d0d5804fc599c32f911
Max.Gronemann <max-gronemann@gmx.de>
2025-12-09 10:03:32 +0100

    Aufgabe Delegates fertig

--- VertiefungCSharp/Delegaten/MitarbeiterVerwaltung.cs
+++ VertiefungCSharp/Delegaten/MitarbeiterVerwaltung.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace Delegaten
 {
+    //Notwendige Klasse
     public class Mitarbeiter
     {
         private string _name;
@@ -13,7 +14,6 @@ namespace Delegaten
         public string Name
         {
             get { return _name; }
-            //Namensänderung geht nur beim Amt!!!XD
         }
         public int Alter
         {
@@ -25,20 +25,26 @@ namespace Delegaten
             get { return _gehalt; }
             set { _gehalt = value; }
         }
-        public Mitarbeiter(string name,int alter, decimal gehalt)
+        public Mitarbeiter(string name, int alter, decimal gehalt)
         {
             _alter = alter;
             _name = name;
             _gehalt = gehalt;
         }
+        public override string ToString()
+        {
+            return $"{Name,-25} : {Alter,5}   {Gehalt,9}";
+        }
     }
+    //Delegate
     public delegate bool MitarbeiterFilter(Mitarbeiter mitarbeiter);
+    //Aufgabe
     public class MitarbeiterVerwaltung
     {
         public static List<Mitarbeiter> filternMitarbeiter(List<Mitarbeiter> liste, MitarbeiterFilter filter)
         {
             List<Mitarbeiter> ergebnis = new List<Mitarbeiter>();
-            foreach(Mitarbeiter m in liste)
+            foreach (Mitarbeiter m in liste)
             {
                 if (filter(m))
                 {
@@ -46,11 +52,43 @@ namespace Delegaten
                 }
             }
             return ergebnis;
-         }
+        }
+        public static List<Mitarbeiter> SortierenMitarbeiter(List<Mitarbeiter> liste, Comparison<Mitarbeiter> filter)
+        {
+            liste.Sort(filter);
+            return liste;
+        }
+        //Weiterleitung an die Main
         public static void TuWas()
         {
             Comparison<Mitarbeiter> nachGehalt = (m1, m2) => m1.Gehalt.CompareTo(m2.Gehalt);
+            List<Mitarbeiter> liste = new List<Mitarbeiter>
+            {
+                new Mitarbeiter("Donald Duck", 35, 3500.00m),
+                new Mitarbeiter("Dagobert Duck", 75, 100000.00m),
+                new Mitarbeiter("Daisy Duck", 32, 3200.00m),
+                new Mitarbeiter("Darkwing Duck", 40, 6000.00m),
+                new Mitarbeiter("Daniel Düsentrieb", 65, 7500.00m),
+                new Mitarbeiter("Mac Moneysac", 70, 95000.00m),
+                new Mitarbeiter("Gustav Gans", 38, 2000.00m),
+                new Mitarbeiter("Bubba", 30, 1800.00m),
+                new Mitarbeiter("Tick Duck", 10, 0.00m),
+                new Mitarbeiter("Trick Duck", 10, 0.00m),
+                new Mitarbeiter("Track Duck", 10, 0.00m)
+            };
+            List<Mitarbeiter> gefiltert = MitarbeiterVerwaltung.filternMitarbeiter(liste, ma => ma.Gehalt > 50000);
 
+            //foreach (Mitarbeiter m in gefiltert)
+            //{
+            //    Console.WriteLine(m);
+            //}
+
+            List<Mitarbeiter> neu = MitarbeiterVerwaltung.SortierenMitarbeiter(liste, (m1,m2) => m1.Alter.CompareTo(m2.Alter));
+
+            foreach (Mitarbeiter m in neu)
+            {
+                Console.WriteLine(m);
+            }
         }
     }
 }
