bffc2ed3c1cd0c57d9c032fdc64306ed11d1db54
Max.Gronemann <max-gronemann@gmx.de>
2025-12-12 14:21:12 +0100

    WarpKern

---
+++ VertiefungCSharp/EventsDO8/Beispiel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsDO8
+{
+    public class MyEventArgs : EventArgs
+    {
+        public string Operation { get; }
+        public string Value { get; }
+        public MyEventArgs(string op, string val)
+        {
+            Operation = op;
+            Value = val;
+        }
+    }
+    public class Repository
+    {
+        private List<string> _dataList;
+
+        //private Action _dataChanged;
+        //public event Action DataChanged
+        //{
+        //    add { _dataChanged += value; }
+        //    remove
+        //    {
+        //            _dataChanged -= value;
+        //    }
+        //}
+        //public event Action DataChanged;
+        public event EventHandler<MyEventArgs> DataChanged;
+        public Repository()
+        {
+            _dataList = new List<string>()
+            {
+                "Hallo",
+                "C#"
+            };
+        }
+        public void AddData(string s)
+        {
+            _dataList.Add(s);
+            //_dataChanged.Invoke();
+            DataChanged(this, new MyEventArgs("Add", s));
+        }
+        public void RemoveData(string s)
+        {
+            _dataList.Remove(s);
+            //_dataChanged.Invoke();
+            DataChanged(this, new MyEventArgs("Remove", s));
+        }
+        public List<string> GetAllData() { return _dataList; }
+    }
+    public class View
+    {
+        private Repository _repo;
+        public View(Repository repository)
+        {
+            this._repo = repository;
+            _repo.DataChanged += RepoDataChanged;
+
+        }
+        public void RepoDataChanged(object sender, MyEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine("Letzte Aktion:");
+            Console.WriteLine(e.Operation + " " + e.Value);
+            Console.WriteLine(sender.ToString());
+            Console.WriteLine();
+            ShowData();
+        }
+        public void ShowData()
+        {
+            List<string> liste = _repo.GetAllData();
+
+            //Console.Clear();
+            Console.WriteLine("Datenbestand:");
+
+            foreach (string s in liste)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+    internal class Beispiel
+    {
+        public static void TuWas()
+        {
+            Repository r = new Repository();
+            View v = new View(r);
+
+            v.ShowData();
+
+            string eingabe;
+            do
+            {
+                eingabe = Console.ReadLine() ?? string.Empty;//konstrukt für Compiler
+                if (eingabe != string.Empty)
+                {
+                    if (eingabe[0] == '-')
+                    {
+                        r.RemoveData(eingabe.Substring(1));
+                    }
+                    else
+                    {
+                        r.AddData(eingabe);
+                    }
+                }
+            } while (eingabe != string.Empty);
+        }
+    }
+}
---
+++ VertiefungCSharp/EventsDO8/StarFleet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsDO8
+{
+    public class WarpKern
+    {
+        private int _warpkernTemperatur = 0;//laut memoryAlpha 2.500.000°K startteperatur
+        public int WarpkernTemperatur { get { return _warpkernTemperatur; } }//Property
+        //ausführliche Variante
+        private EventHandler _tempChange;
+        public event EventHandler TempChange
+        {
+            add { _tempChange += value; }
+            remove { _tempChange -= value; }
+        }
+
+        //Kurze schreibweise
+        public event EventHandler Alert;
+
+        public void ändereTemperatur()
+        {
+            Random rnd = new Random();
+            int änderung = rnd.Next(-100, 201);
+            if ((_warpkernTemperatur + änderung) > 0)
+            {
+                _warpkernTemperatur += änderung;
+                _tempChange.Invoke(this, new EventArgs());
+                if (_warpkernTemperatur >= 500)
+                {
+                    Alert.Invoke(this, new EventArgs());
+                }
+            }
+        }
+    }
+
+    public class WarpKernKonsole
+    {
+        private WarpKern _kern;
+        public WarpKernKonsole(WarpKern warpKern)
+        {
+            _kern = warpKern;
+            _kern.TempChange += Anzeige;
+            _kern.Alert += Warnung;
+        }
+        public void Anzeige(object sender, EventArgs e)
+        {
+            //WarpKern temp = sender as WarpKern;//möglichkeit zur verwendung der Object Sender
+            Console.WriteLine("Der Warpkern hat eine Temperatur von: " + _kern.WarpkernTemperatur);
+            Console.WriteLine("Zeit: " + DateTime.Now);
+        }
+        public void Warnung(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("RED ALERT!!!");
+            Console.ResetColor();
+        }
+    }
+    internal class StarFleet
+    {
+        public static void TuWas()
+        {
+            WarpKern kern = new WarpKern();
+            WarpKernKonsole konsole = new WarpKernKonsole(kern);
+            string eingabe = "";
+            do
+            {
+                eingabe = Console.ReadLine();
+                kern.ändereTemperatur();
+            } while (eingabe != "stop");
+        }
+    }
+}
