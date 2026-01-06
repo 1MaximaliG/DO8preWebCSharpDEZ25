d01885cc015a7adb8b53077703aa6863881c1cfc
Max.Gronemann <max-gronemann@gmx.de>
2026-01-06 15:34:41 +0100

    Frühstück fertig

--- VertiefungCSharp/Threads/BeispielFrühstück.cs
+++ VertiefungCSharp/Threads/BeispielFrühstück.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Threads
 {
@@ -28,40 +29,48 @@ namespace Threads
                 Console.WriteLine("Putting butter on the toast");
             }
 
-            private static Toast ToastBread(int slices)
+            private static async Task<Toast> ToastBreadAsync(int slices)
             {
                 for (int slice = 0; slice < slices; slice++)
                 {
                     Console.WriteLine("Putting a slice of bread in the toaster");
                 }
                 Console.WriteLine("Start toasting...");
-                Task.Delay(3000).Wait();
+                await Task.Delay(3000);
                 Console.WriteLine("Remove toast from toaster");
                 return new Toast();
             }
+            private static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+            {
+                var toast = await ToastBreadAsync(number);
+                ApplyButter(toast);
+                ApplyJam(toast);
 
-            private static Bacon FryBacon(int slices)
+                return toast;
+            }
+
+            private static async Task<Bacon> FryBaconAsync(int slices)
             {
                 Console.WriteLine($"putting {slices} slices of bacon in the pan");
                 Console.WriteLine("cooking first side of bacon...");
-                Task.Delay(3000).Wait();
+                await Task.Delay(3000);
                 for (int slice = 0; slice < slices; slice++)
                 {
                     Console.WriteLine("flipping a slice of bacon");
                 }
                 Console.WriteLine("cooking the second side of bacon...");
-                Task.Delay(3000).Wait();
+                await Task.Delay(3000);
                 Console.WriteLine("Put bacon on plate");
                 return new Bacon();
             }
 
-            private static Egg FryEggs(int howMany)
+            private static async Task<Egg> FryEggsAsync(int howMany)
             {
                 Console.WriteLine("Warming the egg pan...");
-                Task.Delay(3000).Wait();
+                await Task.Delay(3000);
                 Console.WriteLine($"cracking {howMany} eggs");
                 Console.WriteLine("cooking the eggs ...");
-                Task.Delay(3000).Wait();
+                await Task.Delay(3000);
                 Console.WriteLine("Put eggs on plate");
                 return new Egg();
             }
@@ -71,7 +80,7 @@ namespace Threads
                 return new Coffee();
             }
 
-            public static void TuWas()//Main
+            public static async Task TuWas()//Main
             {
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
@@ -79,16 +88,22 @@ namespace Threads
                 Coffee cup = PourCoffee();
                 Console.WriteLine("coffee is ready");
 
-                Egg eggs = FryEggs(2);
-                Console.WriteLine("eggs are ready");
+                Task<Egg> eggsTask = FryEggsAsync(2);
+                Task<Bacon> baconTask = FryBaconAsync(3);
+                Task<Toast> toastTask = MakeToastWithButterAndJamAsync(2);
 
-                Bacon bacon = FryBacon(3);
-                Console.WriteLine("bacon is ready");
-
-                Toast toast = ToastBread(2);
-                ApplyButter(toast);
-                ApplyJam(toast);
-                Console.WriteLine("toast is ready");
+                List<Task> breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+                while (breakfastTasks.Count > 0)
+                {
+                    Task finished = await Task.WhenAny(breakfastTasks);
+                    if(finished == eggsTask)
+                        Console.WriteLine("eggs are ready");
+                    else if(finished == baconTask)
+                        Console.WriteLine("bacon is ready");
+                    else
+                        Console.WriteLine("toast is ready");
+                    breakfastTasks.Remove(finished);
+                }
 
                 Juice oj = PourOJ();
                 Console.WriteLine("oj is ready");
---
+++ VertiefungCSharp/Threads/ThreadGrundlagen.cs
@@ -0,0 +1,97 @@
+﻿namespace Threads
+{
+    internal partial class Program
+    {
+        public class ThreadGrundlagen
+        {
+            //a)	Schreiben Sie eine statische Methode, die in einer for-Schleife
+            //      100 mal Informationen über den aktuellen Thread ausgibt. Dazu
+            //      sollen die ManagedThreadId, die Priorität und der Status des
+            //      Threads gehören. Zusätzlich soll die Methode innerhalb der Schleife
+            //      eine zufällige Zeit zwischen 100 und 500 Millisekunden „schlafen“.
+            //      Nach Beendigung der Schleife soll die Methode dann ausgeben, dass
+            //      sie das Ende erreicht hat.
+            public static void MyMethodA(CancellationToken token)
+            {
+                Random rng = new Random();
+                // e) & f)
+                int x = rng.Next(0, 4);
+                for (int i = 0; i < 100; i++)
+                {
+                    if (token.IsCancellationRequested && x % 3 == 0)
+                    {
+                        //Console.WriteLine($"Beende : {Environment.CurrentManagedThreadId} Status {Thread.CurrentThread.ThreadState}");
+                        break;
+                    }
+                    Console.WriteLine($"ID:{Environment.CurrentManagedThreadId}," +
+                        $"   Priorität: {Thread.CurrentThread.Priority}," +
+                        $"     Status: {Thread.CurrentThread.ThreadState}");
+
+                    Thread.Sleep(rng.Next(100, 501));
+                }
+                Console.WriteLine("ENDE!");
+            }
+            public static void TuWas()//Main
+            {
+                //a)
+                //MyMethodA();
+
+                //b)	Legen Sie im Hauptprogramm eine Liste von Threads an und fügen Sie
+                //      dieser Liste 10 Threads hinzu, die alle Ihre Methode mit der
+                //      Schleife starten. Starten Sie alle Threads in der Liste.
+
+                //c)	Erweitern Sie das Hauptprogramm, so dass nach dem Start aller Threads
+                //      ein zufälliger Thread aus der Liste ausgewählt und abgebrochen wird.
+                //      Der abgebrochene Thread soll dann aus der Liste entfernt werden.
+                //      Dieser Vorgang soll sich nach einer zufälligen Wartezeit zwischen 100
+                //      und 500 Millisekunden solange wiederholen, bis keine Threads mehr in
+                //      der Liste vorhanden sind.
+
+                List<CancellationTokenSource> ctsList = new List<CancellationTokenSource>();
+                for (int i = 0; i < 10; i++)
+                {
+                    ctsList.Add(new CancellationTokenSource());
+                }
+
+                List<Thread> threadList = new List<Thread>();
+                for (int i = 0; i < 10; i++)
+                {
+                    CancellationTokenSource temp = ctsList[i];
+                    threadList.Add(new Thread(() => MyMethodA(temp.Token)));
+                }
+                foreach (Thread t in threadList)
+                {
+                    t.Start();
+                }
+                Random rng = new Random();
+                do
+                {
+                    int n = rng.Next(threadList.Count);
+
+                    ctsList[n].Cancel();
+                    Thread.Sleep(rng.Next(1000, 5001));
+                    ctsList.RemoveAt(n);
+                    //d)
+                    Console.WriteLine($"Thread{threadList[n].ManagedThreadId} => {threadList[n].ThreadState}");
+                    threadList.RemoveAt(n);
+                } while (threadList.Count > 0);
+
+                //d)	Erweitern Sie Ihre Methode, so dass sie im Falle eines Abbruchs von
+                //      außen eine passende Meldung mit der ManagedThreadId und dem
+                //      Thread - Status ausgibt.
+
+
+                //e)	Erweitern Sie Ihre Methode noch einmal, so dass sie die(zufällige)
+                //      Möglichkeit hat den Abbruch zu ignorieren und bis zum Ende
+                //      weiterzulaufen.In diesem Fall soll am Ende wieder die Meldung
+                //      ausgegeben werden, dass die Methode das Ende erreicht hat.
+
+
+                //f)	Erhöhen Sie die Chance, dass die Methode den Abbruch ignoriert
+                //      auf 75 %.
+
+
+            }
+        }
+    }
+}
