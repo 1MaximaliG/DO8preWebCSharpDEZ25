19d739d19e346783e3fca73a503a22768740e059
Max.Gronemann <max-gronemann@gmx.de>
2026-01-08 15:53:56 +0100

    Serializierung 2

--- VertiefungCSharp/Serien/Program.cs
+++ VertiefungCSharp/Serien/Program.cs
@@ -93,7 +93,6 @@ namespace Serien
             {
                 Console.WriteLine(file + " exisitiert nicht!!!");
             }
-
         }
         public static void Speichern()
         {
@@ -111,10 +110,8 @@ namespace Serien
 
             File.WriteAllText(file, serialisiertePerson);
         }
-        static void Main(string[] args)
+        public static void Personen01Aufgabe()
         {
-            //XmlExample();
-            //JsonExample();
             while (true)
             {
                 Console.WriteLine("1: Laden");
@@ -138,6 +135,98 @@ namespace Serien
                         break;
                 }
             }
+        }
+        public class Person02
+        {
+            //Felder
+            public string Vorname { get; set; }
+            public string Nachname { get; set; }
+            public DateTime Geburtsdatum { get; set; }
+            public Person02 Mudder { get; set; }
+            public Person02 Vadder { get; set; }
+            public Person02 Blag { get; set; }
+
+            public Person02() { }
+            public Person02(string name, string nachname, DateTime geburtstag)
+            {
+                Vorname = name;
+                Nachname = nachname;
+                Geburtsdatum = geburtstag;
+            }
+            public override string ToString()
+            {
+                return Vorname + " " + Nachname + " " + Geburtsdatum.ToShortDateString();
+            }
+        }
+        public static void Personen02Aufgabe()
+        {
+            //A
+            List<Person02> erstellen = new List<Person02>() {
+                new Person02("Bilbo","Beutlin",new DateTime(1290,9,22)),
+                new Person02("James Tiberius","Kirk",new DateTime(2233,3,22)),
+                new Person02("Donald","Trump",new DateTime(1946,6,14)),
+                new Person02("Jong-Il","Kim",new DateTime(1941,2,16)),
+                new Person02("Albus Percival Wulfric Brian","Dumbledor",new DateTime(1881,8,18))
+            };
+            string file = "DieListe.json";
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            string formatiert = JsonSerializer.Serialize(erstellen,options);
+            File.WriteAllText(file,formatiert);
+            erstellen = null;//Liste ist jetzt Leer
+            string inhalt = File.ReadAllText(file);
+            erstellen = JsonSerializer.Deserialize<List<Person02>>(inhalt);
+
+            foreach(var p in erstellen)
+            {
+                Console.WriteLine(p);
+            }
+            //B
+            Person02 eins = new Person02("Abc","A",new DateTime(1234,1,5));
+            Person02 zwei = new Person02("Def", "B", new DateTime(1234, 1, 5));
+            Person02 kind = new Person02("Gah", "A", new DateTime(1234, 1, 5));
+            kind.Mudder = eins;
+            eins.Blag = kind;
+            kind.Vadder = zwei;
+            zwei.Blag = kind;
+            Person02 ommaA = new Person02("Ijk", "A", new DateTime(1234, 1, 5));
+            eins.Mudder = ommaA;
+            ommaA.Blag = eins;
+            Person02 ommaB = new Person02("Lmn", "B", new DateTime(1234, 1, 5));
+            zwei.Mudder = ommaB;
+            ommaB.Blag = zwei;
+            Person02 oppaA = new Person02("Opq", "A", new DateTime(1234, 1, 5));
+            eins.Vadder = oppaA;
+            oppaA.Blag = eins;
+            Person02 oppaB = new Person02("Rst", "B", new DateTime(1234, 1, 5));
+            oppaB.Blag = zwei;
+            zwei.Vadder = oppaB;
+            JsonSerializerOptions option = new JsonSerializerOptions() 
+            { 
+                WriteIndented = true ,
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+            File.WriteAllText("eins.json", JsonSerializer.Serialize(eins,option));
+            File.WriteAllText("zwei.json", JsonSerializer.Serialize(zwei,option));
+            File.WriteAllText("drei.json", JsonSerializer.Serialize(kind,option));
+            File.WriteAllText("vier.json", JsonSerializer.Serialize(ommaA,option));
+            File.WriteAllText("fuenf.json", JsonSerializer.Serialize(ommaB,option));
+            File.WriteAllText("sechs.json", JsonSerializer.Serialize(oppaA,option));
+            File.WriteAllText("sieben.json", JsonSerializer.Serialize(oppaB,option));
+            Person02 person1 = JsonSerializer.Deserialize<Person02>(File.ReadAllText("sieben.json"));
+            Person02 person2 = JsonSerializer.Deserialize<Person02>(File.ReadAllText("sechs.json"));
+            Console.WriteLine(person1.Blag.Blag);
+            Console.WriteLine(person2.Blag.Blag);
+            Console.WriteLine(person1.Blag.Blag== person2.Blag.Blag);
+
+
+        }
+        static void Main(string[] args)
+        {
+            //XmlExample();
+            //JsonExample();
+            //Personen01Aufgabe();
+            Personen02Aufgabe();
+            
 
         }
     }
